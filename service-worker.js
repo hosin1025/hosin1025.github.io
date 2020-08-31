11b6554512d97ea8badadcc8bc49e5a3f31d38bd
hosin1025 <70520904+hosin1025@users.noreply.github.com>
2020-08-31 22:40:20 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "static-cache-v1.0.2";
+const FILES_TO_CACHE = [
+    "/index.html",
+    "/favicon.ico",
+    "/main.js",
+    "/style.css",
+    "/logo/logo-152.png",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) =>
+            Promise.all(
+                keyList.map((key) => {
+                    if (CACHE_NAME !== key) return caches.delete(key);
+                })
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        });
+    });
+});
